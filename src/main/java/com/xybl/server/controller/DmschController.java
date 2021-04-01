111faa5a5baaa7471b2928ae598ba540119f6c68
Liu Bocai <44741902+liubocai@users.noreply.github.com>
2021-04-01 20:03:59 +0800

    add api for get super department

--- src/main/java/com/xybl/server/controller/DmschController.java
+++ src/main/java/com/xybl/server/controller/DmschController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -159,5 +160,45 @@ public class DmschController {
                 return response(400, "update failed");
             }
         }
+    }
+
+    @RequestMapping("/getsuperdm")
+    Map<String, Object> getAllSuperDm(@RequestParam(name = "id")String id){
+        //1.接收参数
+        User tuser = userService.getUserById(id);
+        String tname = tuser.getName();
+        //2.查询super机构
+        List<String> tnameList = new ArrayList<>();
+        List<String> uidList = new ArrayList<>();
+        String s1 = tname.substring(0, 2);
+        String s2 = tname.substring(2, 4);
+        String s3 = tname.substring(4, 7);
+        String s4 = tname.substring(7, 10);
+        String s5 = tname.substring(10, 13);
+
+        if(!"00".equals(s1)){
+            tnameList.add("0000000000000");
+        }
+        if(!"00".equals(s2)){
+            tnameList.add(s1+"00000000000");
+        }
+        if(!"000".equals(s3)){
+            tnameList.add(s1+s2+"000000000");
+        }
+        if(!"000".equals(s4)){
+            tnameList.add(s1+s2+s3+"000000");
+        }
+        if(!"000".equals(s5)){
+            tnameList.add(s1+s2+s3+s4+"000");
+        }
+
+
+        for(String s : tnameList){
+            String t = userService.getUidByNid(s);
+            if(t != null){
+                uidList.add(t);
+            }
+        }
+        return response(200, "ok", uidList);
     }
 }
